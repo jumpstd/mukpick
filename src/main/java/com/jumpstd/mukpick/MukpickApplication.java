e83f19163a7d6bfa62d8b3ff06d753b779f73eb7
seoyyyy <seoyyyy@naver.com>
2021-07-04 19:02:14 +0900

    첫 커밋

---
+++ src/main/java/com/jumpstd/mukpick/MukpickApplication.java
@@ -0,0 +1,13 @@
+package com.jumpstd.mukpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumpstd/mukpick/MukpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumpstd.mukpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
